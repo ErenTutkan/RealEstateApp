972e01bcb31d4325786da2afc3af67acfacfde7d
Eren Tutkan <erentutkan@gmail.com>
2022-09-10 19:38:47 +0300

    Proje dosyası ekle.

---
+++ RealEstateApp.Core/Models/Estate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateApp.Core.Models
+{
+    public class Estate:IEntity
+    {
+        public string Title { get; set; }
+        public int BedSize { get; set; }
+        public int BathSize { get; set; }
+        public string? Address { get; set; }
+        public string? Details { get; set; }
+        public string RoomSize { get; set; }
+        public Image Image { get; set; }
+
+    }
+}
---
+++ RealEstateApp.Core/Models/IEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstateApp.Core.Models
+{
+    public class IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime CreateTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
